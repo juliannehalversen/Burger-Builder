f1fafb421650c09d2eefc86a63045e5c62a0c1d1
Julianne Lee <juliannehalversen@gmail.com>
2020-02-24 13:23:07 -0700

    Added toolbar

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -71,8 +71,12 @@ class BurgerBuilder extends Component {
         this.setState({purchasing: true});
     };
 
-    purchaseCandelHandler = () => {
+    purchaseCancelHandler = () => {
         this.setState({ purchasing: false });
+    };
+
+    purchaseContinueHandler = () => {
+        alert('You continue!');
     };
 
     render() {
@@ -86,8 +90,13 @@ class BurgerBuilder extends Component {
         return(
             <Aux>
                 <Modal show={this.state.purchasing} modalClosed={this.purchaseCandelHandler}>
-                    <OrderSummary ingredients={this.state.ingredients} />
-                    
+                    <OrderSummary 
+                        ingredients={this.state.ingredients} 
+                        purchaseCancelled={this.purchaseCancelHandler}
+                        purchaseContinued={this.purchaseContinueHandler}
+                        price={this.state.totalPrice}
+                    />
+
                 </Modal>
                 <Burger ingredients={this.state.ingredients}/>
                 <BuildControls 
