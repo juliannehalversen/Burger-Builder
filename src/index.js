367abc2f23e561a53a48700697802e48208a1d97
Julianne Lee <juliannehalversen@gmail.com>
2020-04-08 11:40:40 -0600

    removed extra semi colon

--- src/index.js
+++ src/index.js
@@ -25,7 +25,7 @@ const store = createStore(rootReducer, composeEnhancers(
 const app = (
     <Provider store={store}>
         <BrowserRouter>
-            <App />;
+            <App />
         </BrowserRouter>
     </Provider>
 )
